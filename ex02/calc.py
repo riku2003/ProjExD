392c0194d5744dfd0139928b378c929fa47a9a44
c0a2100529 <c0A2100529@edu.teu.ac.jp>
2022-11-29 16:45:04 +0900

    電卓

--- ex02/calc.py
+++ ex02/calc.py
@@ -9,26 +9,18 @@ def button_click(event):
         res = eval(keisan)
         entry.delete(0,tk.END)
         entry.insert(tk.END,res)
+
+    elif i == "C":
+        entry.delete(0,tk.END) 
+    
     else:
         entry.insert(tk.END,i)
-
+        
 root = tk.Tk()
 root.geometry("300x500")
 
 entry = tk.Entry(root,justify="right",width=10,font=("",40))
 entry.grid(row=0,column=0,columnspan=3)
-
-#練習2
-#button = tk.Button(root,text="9",width=4,height=2,font=("",30)).pack()
-#button = tk.Button(root,text="8",width=4,height=2,font=("",30)).pack()
-#button = tk.Button(root,text="7",width=4,height=2,font=("",30)).pack()
-#button = tk.Button(root,text="6",width=4,height=2,font=("",30)).pack()
-#button = tk.Button(root,text="5",width=4,height=2,font=("",30)).pack()
-#button = tk.Button(root,text="4",width=4,height=2,font=("",30)).pack()
-#button = tk.Button(root,text="3",width=4,height=2,font=("",30)).pack()
-#button = tk.Button(root,text="2",width=4,height=2,font=("",30)).pack()
-#button = tk.Button(root,text="1",width=4,height=2,font=("",30)).pack()
-#button = tk.Button(root,text="0",width=4,height=2,font=("",30)).pack()
 
 r, c = 1, 0
 for i in range(9, -1, -1):
@@ -49,5 +41,14 @@ for ope in operators:
     if c%3 == 0:
         r += 1
         c = 0
+
+button = tk.Button(root,text="C",width=4,height=2,font=("",30))
+button.grid(row=r,column=c)
+button.bind("<1>",button_click)
+c += 1
+if c%3 ==0:
+    r += 1
+    c =0
+
 
 root.mainloop()
