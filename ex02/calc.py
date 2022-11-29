d9c4365c9b3695324be260bf35b8f42af5fa9c38
c0a2100529 <c0A2100529@edu.teu.ac.jp>
2022-11-29 17:43:01 +0900

    電卓 #5

--- ex02/calc.py
+++ ex02/calc.py
@@ -5,12 +5,12 @@ def button_click(event):
     btn = event.widget
     i = btn["text"]
     if i == "=":
-        keisan = entry.get()
-        res = eval(keisan)
-        entry.delete(0,tk.END)
-        entry.insert(tk.END,res)
+        keisan = entry.get() # 数式の文字列
+        res = eval(keisan) # 数式文字列の評価
+        entry.delete(0,tk.END) # 表示文字列の削除
+        entry.insert(tk.END,res) # 結果の挿入
 
-    elif i == "C":
+    elif i == "C": #クリアの設定
         entry.delete(0,tk.END) 
     
     else:
@@ -23,7 +23,7 @@ entry = tk.Entry(root,justify="right",width=10,font=("",40))
 entry.grid(row=0,column=0,columnspan=3)
 
 r, c = 1, 0
-for i in range(9, -1, -1):
+for i in range(9, -1, -1): #数字キーのボタン設定
     button = tk.Button(root, text=f"{i}",width=4,height=2,font=("",30))
     button.grid(row=r,column=c)
     button.bind("<1>",button_click)
@@ -32,7 +32,7 @@ for i in range(9, -1, -1):
         r += 1
         c = 0
 
-operators = ["+","=","C"]
+operators = ["+","=","C"] #+,=,Cのボタン設定
 for ope in operators:
     button = tk.Button(root, text=f"{ope}",width=4,height=2,font=("",30))
     button.grid(row=r,column=c)
@@ -41,6 +41,5 @@ for ope in operators:
     if c%3 == 0:
         r += 1
         c = 0
-
 
 root.mainloop()
